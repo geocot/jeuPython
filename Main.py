9e1c7bcc626d3e205632201914235a1cd770daf5
Professeur Cot <martin.couture@uqtr.ca>
2025-02-20 20:44:23 -0500

    Base de l'affichage

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
 #Martin Couture
+import pygame
+
+pygame.init()
+
+HAUTEUR_FENETRE = 800
+LARGEUR_FENETRE = 1200
+COULEUR_FOND = (0,0,0)
+ECRAN = pygame.display.set_mode((LARGEUR_FENETRE, HAUTEUR_FENETRE))
+
+arretJeu = False
+
+while not arretJeu:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+            if event.key == pygame.K_ESCAPE:
+                arretJeu = True
